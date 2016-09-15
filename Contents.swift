7b68ba5b696ca179afef180c0dbc28401ecf3e75
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-15 09:37:15 -0600

    Just Goofed off

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var reply = "Whats good Sunny D"
+var x = 7, y = 300, z = 666
+var number = 123456
+print(number)
+print(number + 12)
+
+print("\(number)" + str)
+
+print (x + z - y)
+
